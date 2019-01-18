404aa70e90efeae4766c4abe7a5795d98f580ea8
John Sterlacci <johntsterlacci@gmail.com>
2019-01-18 12:00:20 -0500

    added rpc on 8545

--- geth/geth-master.sh
+++ geth/geth-master.sh
@@ -7,5 +7,4 @@ ACCOUNT="$(geth --datadir="usr/local/bin/build/master" account list | awk '$2=="
 #Start up local geth 
 echo Firing up engines
 geth --datadir="usr/local/bin/build/master" init usr/local/bin/genesisBlock.json
-geth --datadir="usr/local/bin/build/master" --etherbase "0x$ACCOUNT" --port 30302 --ipcdisable --wsaddr 0.0.0.0 --ws --wsport 8546 --wsorigins "*" --mine --syncmode "full"  --networkid 1370 --unlock "0x$ACCOUNT" --password usr/local/bin/passkey.txt console
-
+geth --datadir="usr/local/bin/build/master" --etherbase "0x$ACCOUNT" --port 30302 --ipcdisable --ws --wsaddr 0.0.0.0 --wsport 8546 --wsorigins "*" --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --mine --syncmode "full"  --networkid 1370 --unlock "0x$ACCOUNT" --password usr/local/bin/passkey.txt console
